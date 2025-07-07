6d97ba467e9f2f9b942a2bc79fc005b4278fa62e
SonNPXSE183201 <sonnpxse183201@fpt.edu.vn>
2025-07-07 20:56:53 +0700

    Sửa lại BaseEntity

--- src/main/java/com/ferticare/ferticareback/common/entity/BaseEntity.java
+++ src/main/java/com/ferticare/ferticareback/common/entity/BaseEntity.java
@@ -2,11 +2,8 @@ package com.ferticare.ferticareback.common.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ferticare.ferticareback.common.constant.DataPatternConstant;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Column;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
@@ -17,6 +14,8 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -32,35 +31,51 @@ public abstract class BaseEntity implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
-
     @CreatedBy
-    @Column(updatable = false)
+    @Column(name = "created_by", updatable = false)
     private String createdBy;
 
     @LastModifiedBy
+    @Column(name = "updated_by")
     private String updatedBy;
 
     @CreatedDate
-    @Column(updatable = false)
+    @Column(name = "created_at", updatable = false)
     @JsonFormat(pattern = DataPatternConstant.TIMESTAMP_FORMAT)
     private LocalDateTime createdDate;
 
     @LastModifiedDate
+    @Column(name = "updated_at")
     @JsonFormat(pattern = DataPatternConstant.TIMESTAMP_FORMAT)
     private LocalDateTime updatedDate;
 
     @PrePersist
     protected void onCreate() {
-        this.createdDate = LocalDateTime.now();
-        this.updatedDate = LocalDateTime.now();
+        LocalDateTime now = LocalDateTime.now();
+        this.createdDate = now;
+
+        // Tự động set created_by từ user hiện tại
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated() &&
+                !"anonymousUser".equals(authentication.getPrincipal())) {
+            this.createdBy = authentication.getName();
+        } else {
+            this.createdBy = "system";
+        }
     }
 
     @PreUpdate
     protected void onUpdate() {
         this.updatedDate = LocalDateTime.now();
+
+        // Tự động set updated_by từ user hiện tại
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated() &&
+                !"anonymousUser".equals(authentication.getPrincipal())) {
+            this.updatedBy = authentication.getName();
+        } else {
+            this.updatedBy = "system";
+        }
     }
 
 }
